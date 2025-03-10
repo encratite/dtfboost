cf135d20ef9c6f26a7c6e44a4345a9fae4f4d40d
void <git.email.address@googlemail.com>
2025-03-10 03:05:16 +0100

    Implemented basic LightGBM binary classifier training

--- train.py
+++ train.py
@@ -1,13 +1,23 @@
-from typing import Any
 import os
 import sys
+import random
 from collections import defaultdict
 import pandas as pd
 import numpy as np
 import numpy.typing as npt
+from sklearn.preprocessing import StandardScaler
+import lightgbm as lgb
+from sklearn.metrics import roc_auc_score, f1_score, precision_score, confusion_matrix
 from series import TimeSeries
 from ohlc import OHLC
 from config import Configuration
+
+class TrainingData:
+	ohlc_series: TimeSeries[OHLC]
+
+	def __init__(self, symbol: str):
+		path = os.path.join(Configuration.CONTINUOUS_CONTRACT_DIRECTORY, f"{symbol}.csv")
+		self.ohlc_series = TimeSeries.read_ohlc_csv(path)
 
 def get_rate_of_change(new_value: float | int, old_value: float | int):
 	rate = float(new_value) / float(old_value) - 1.0
@@ -15,10 +25,8 @@ def get_rate_of_change(new_value: float | int, old_value: float | int):
 	rate = min(rate, 1.0)
 	return rate
 
-def get_features(symbol: str, start: pd.Timestamp, end: pd.Timestamp) -> None:
+def get_features(data: TrainingData, start: pd.Timestamp, end: pd.Timestamp, balanced_samples: bool, shuffle: bool) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.int8]]:
 	assert start < end
-	path = os.path.join(Configuration.CONTINUOUS_CONTRACT_DIRECTORY, f"{symbol}.csv")
-	ohlc_series = TimeSeries.read_ohlc_csv(path)
 	offsets = [
 		0,
 		1,
@@ -31,13 +39,13 @@ def get_features(symbol: str, start: pd.Timestamp, end: pd.Timestamp) -> None:
 	]
 	max_offset = max(offsets)
 	i = 0
-	unbalanced_features: defaultdict[int, list[list[float | int]]] = defaultdict(list)
-	for time in ohlc_series:
+	unbalanced_features: defaultdict[int, list[list[float]]] = defaultdict(list)
+	for time in data.ohlc_series:
 		if time >= end:
 			break
 		if i > max_offset and start <= time:
-			records: list[OHLC] = ohlc_series.get(time, offsets=offsets)
-			close_values = list(map(lambda ohlc: ohlc.close, records))
+			records: list[OHLC] = data.ohlc_series.get(time, offsets=offsets)
+			close_values = [ohlc.close for ohlc in records]
 			today = close_values[0]
 			yesterday = close_values[1]
 			momentum_close_values = close_values[2:]
@@ -48,19 +56,86 @@ def get_features(symbol: str, start: pd.Timestamp, end: pd.Timestamp) -> None:
 			label = 1 if label_rate > 0 else 0
 			unbalanced_features[label].append(features)
 		i += 1
-	for label, label_features in unbalanced_features.items():
-		print(f"Label {label}: {len(label_features)}")
-		for f in label_features[0:5]:
-			print(f)
+
+	# Try to create a balanced data set by adding samples from the smaller class
+	class_sizes = sorted(unbalanced_features.items(), key=lambda x: len(x[1]))
+	assert len(class_sizes) == 2
+	small_class_label, small_class_features = class_sizes[0]
+	large_class_label, large_class_features = class_sizes[1]
+	if balanced_samples:
+		imbalance = len(large_class_features) - len(small_class_features)
+	else:
+		imbalance = 0
+	if imbalance < len(small_class_features):
+		# The imbalance is small enough to use sampling without replacement
+		additional_samples = random.sample(small_class_features, k=imbalance)
+	else:
+		# The imbalance is too great, use sampling with replacement
+		additional_samples = random.choices(small_class_features, k=imbalance)
+	small_class_features += additional_samples
+	balanced_data = [(x, small_class_label) for x in small_class_features] + [(x, large_class_label) for x in large_class_features]
+	if shuffle:
+		# Randomize the order of the samples (not sure if relevant for most algorithms)
+		random.shuffle(balanced_data)
+	features = [x[0] for x in balanced_data]
+	np_features = np.array(features, dtype=np.float64)
+	labels = [x[1] for x in balanced_data]
+	np_labels = np.array(labels, dtype=np.int8)
+	return np_features, np_labels
+
+def train(symbol: str, start: pd.Timestamp, split: pd.Timestamp, end: pd.Timestamp) -> None:
+	assert start < split < end
+	data = TrainingData(symbol)
+	x_train, y_train = get_features(data, start, split, True, True)
+	x_validation, y_validation = get_features(data, split, end, False, False)
+	scaler = StandardScaler()
+	scaler.fit(x_train)
+	x_train = scaler.transform(x_train)
+	x_validation = scaler.transform(x_validation)
+
+	# Hyperparameters
+	num_iterations_values = [50, 100, 200, 500, 1000]
+	for num_iterations in num_iterations_values:
+		params = {
+			"verbosity": -1,
+			"objective": "binary",
+			"metric": ["binary_logloss", "auc"],
+			"num_iterations": num_iterations
+		}
+		train_dataset = lgb.Dataset(x_train, label=y_train)
+		validation_dataset = lgb.Dataset(x_validation, label=y_validation, reference=train_dataset)
+		model = lgb.train(params, train_dataset, valid_sets=[validation_dataset])
+
+		print(f"num_iterations: {num_iterations}")
+		print_metrics("Training", model, x_train, y_train)
+		print_metrics("Validation", model, x_validation, y_validation)
+
+def binary_predictions(predictions: np.ndarray[np.float64]) -> np.ndarray[np.int8]:
+	return (predictions > 0.5).astype(dtype=np.int8) # type: ignore
+
+def print_metrics(title, model, x, y):
+	predictions = model.predict(x)
+	predictions = binary_predictions(predictions)
+	precision = precision_score(y, predictions)
+	roc_auc = roc_auc_score(y, predictions)
+	f1 = f1_score(y, predictions)
+	matrix = confusion_matrix(y, predictions)
+	print(f"\t{title}:")
+	print(f"\t\tPrecision: {precision:.1%}")
+	print(f"\t\tROC-AUC: {roc_auc:.3f}")
+	print(f"\t\tF1: {f1:.3f}")
+	print(f"\t\t[{matrix[0][0]:6d} {matrix[0][1]:6d}]")
+	print(f"\t\t[{matrix[1][0]:6d} {matrix[1][1]:6d}]")
 
 def main() -> None:
-	if len(sys.argv) != 4:
+	if len(sys.argv) != 5:
 		print("Usage:")
-		print(f"python {sys.argv[0]} <symbol> <start ISO date> <end ISO date>")
+		print(f"python {sys.argv[0]} <symbol> <start date> <split date> <end date>")
 		return
 	symbol = sys.argv[1]
 	start = pd.Timestamp(sys.argv[2])
-	end = pd.Timestamp(sys.argv[3])
-	get_features(symbol, start, end)
+	split = pd.Timestamp(sys.argv[3])
+	end = pd.Timestamp(sys.argv[4])
+	train(symbol, start, split, end)
 
 main()
